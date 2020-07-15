0789c77133c2d6f97bae9f0b2cc4c177f240884a
Maciej Barć <xgqt@protonmail.com>
2020-07-15 18:03:14 +0200

    update test suite

--- test.sh
+++ test.sh
@@ -3,60 +3,51 @@
 
 message="
 ------------------------------
-Test results:
+Tests summary:
 
 "
 
 result=0
 
 
-if time ./nf --travis -v
-then
-    message+="Passed the time test"
-else
-    message+="Failed the time test"
-    result=1
-fi
-message+="
+function test_unit() {
+    # $1 - test name
+    # $2 - test command
+
+    echo "Running the '${1}' test..."
+
+    if eval "${2}"
+    then
+        echo "Passed the '${1}' test [OK]"
+        message+="Passed the '${1}' test"
+    else
+        echo "Failed the '${1}' test [!!]"
+        message+="Failed the '${1}' test"
+        result=1
+    fi
+
+    message+="
 Ran:
-     time ./nf --travis -v
+     '${2}'
 
 "
+}
 
-if shellcheck -e SC1117,SC2243,SC2244 nf || exit 1
-then
-    message+="Passed the shellcheck test"
-else
-    message+="Failed the shellcheck test"
-    result=1
-fi
-message+="
-Ran:
-     shellcheck -e SC1117,SC2243,SC2244 nf
 
-"
+test_unit "time" "time ./nf --travis -v"
 
-if ! grep '.\{102\}' nf
-then
-    message+="Passed the line length test"
-else
-    message+="Failed the line length test"
-    result=1
-fi
-message+="
-Ran:
-     ! grep '.\{102\}' nf
+test_unit "shellcheck" "shellcheck -e SC1117,SC2243,SC2244 nf"
 
-"
+test_unit "line length" "! grep '.\{102\}' nf"
 
 
 echo "${message}"
 
 if [ ${result} -eq 0 ]
 then
-    echo "Tests passed"
+    echo "Tests passed [OK]"
 else
-    echo "Tests failed"
+    echo "Tests failed [!!]"
 fi
 echo "------------------------------"
 
